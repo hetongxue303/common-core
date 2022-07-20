de0a14aa85e8f936df2a26953d559aad8201d445
hetongxue303 <1974597686@qq.com>
2022-07-21 00:24:32 +0800

    解决用户注销登录时的问题

--- system/src/main/java/com/hetongxue/security/handler/CustomizeLogoutSuccessHandler.java
+++ system/src/main/java/com/hetongxue/security/handler/CustomizeLogoutSuccessHandler.java
@@ -7,6 +7,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -29,8 +30,15 @@ public class CustomizeLogoutSuccessHandler implements LogoutSuccessHandler {
 
     @Override
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        // 判断是否有权限信息 有的话删除
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
         response.setContentType("application/json;charset=utf-8");
         response.setStatus(HttpStatus.OK.value());
+        // 将token新信息设置为空
+        response.setHeader(Const.AUTHORIZATION_KEY, "");
+        // 清空redis中的token信息
         redisTemplate.delete(Const.AUTHORIZATION_KEY);
         response.getWriter().println(new ObjectMapper().writeValueAsString(Result.Success().setMessage("注销成功")));
     }
